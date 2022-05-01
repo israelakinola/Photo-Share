a112458bd8d357eb319a0116a413c8dfddc9f7e9
Israel <gregsmedia2014@gmail.com>
2022-05-01 19:14:16 -0400

    Update Readme

--- share/tests.py
+++ share/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+
+class FunctionalTestCase(TestCase):
+    """ The testcase for all functional test is writting here """
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+class UnitTestCase(TestCase):
+    """ The testcase for all functional test is wååritting here """
+    pass
