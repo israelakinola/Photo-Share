180686b03f3b7e967850d97d0eab88e3c8e98b9d
Israel <gregsmedia2014@gmail.com>
2022-05-13 18:00:27 -0400

    formatted with black

--- share/forms.py
+++ share/forms.py
@@ -3,8 +3,13 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Photo
 
+
 class PostAPhotoForm(forms.ModelForm):
-    """ This class setup a model from for the Photo model """
-    class Meta():
+    """This class setup a model from for the Photo model"""
+
+    class Meta:
         model = Photo
-        fields = ['url','caption',]
+        fields = [
+            "url",
+            "caption",
+        ]
