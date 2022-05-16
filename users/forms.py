67dce37077995ce961ed62efdbcb8099a362cfe2
Israel <gregsmedia2014@gmail.com>
2022-05-16 02:08:06 -0400

    retrieve

---
+++ users/forms.py
@@ -0,0 +1,15 @@
+from dataclasses import field
+import email
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignupForm(UserCreationForm):
+    """This class extends the User creation form class and add an email field"""
+
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
---
+++ users/views.py
@@ -0,0 +1,24 @@
+import re
+from django.shortcuts import render, HttpResponseRedirect, redirect
+from .forms import SignupForm
+from django.contrib import messages
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+
+def signup(request):
+    """This function handles the Signup view and Form Handling"""
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"User Successfully Created {username}")
+            return redirect("share.home")
+    else:
+        form = SignupForm()
+    return render(request, "users/signup.html", {"form": form})
+
+
+class UsersLoginView(LoginView):
+    template_name = "users/login.html"
