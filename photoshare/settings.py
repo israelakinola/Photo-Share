bf8aa7ab0e7112f16ac22f5b712eb287e87ac4c7
Israel <gregsmedia2014@gmail.com>
2022-05-15 23:36:37 -0400

    Added static root path

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z=hjyq(u1c#qyt7n9$^!td)7k-o@7ajc6%%fi@a^aim+^a6@w3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
